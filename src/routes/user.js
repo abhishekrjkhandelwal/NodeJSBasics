1f2aef4be21f7ade46009ea52b39a9abb6bf4268
abhishekrjkhandelwal <abhishek30199808@gmail.com>
2024-11-14 13:26:32 +0530

    Push Backend Complete Code.

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,8 +3,11 @@ const userRouter = express.Router();
 
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest")
+const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills"
 
-//Get all the pending connection request for the loggedIn user. 
+const User = require("../models/user")
+
+// Get all the pending connection request for the loggedIn user. 
 userRouter.get("/user/requests/recevied", userAuth, async (req, res) => {
     try {
         const loggedInUser = req.user;
@@ -36,6 +39,49 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
         res.json({ data: connectionRequests})
     } catch(err) {
         res.status(400).send({message: err.message})
+    }
+})
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+    try {
+        /**
+         * User should see all the user cards except
+         * 0. his own card
+         * 1. his connections
+         * 2. ignored people
+         * 3. already sent the connection request
+         */
+
+        const loggedInUser = req.user;
+
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 10;
+        const skip = (page - 1) * limit;
+        limit = limit > 50 ? 50 : limit;
+
+        //Find all connection requests (sent + received)
+        const connectionRequest = await ConnectionRequest.find({
+            $or: [{ fromUserId: loggedInUser._id, toUserId: loggedInUser._id }]
+        }).select("fromUserId toUserId")
+
+        const hideUsersFromFeed = new Set();
+        connectionRequest.forEach((req) => {
+            hideUsersFromFeed.add(req.fromUserId.toString());
+            hideUsersFromFeed.add(req.toUserId.toString())
+        });
+
+        console.log(connectionRequest, skip, limit);
+
+        const users = await User.find({
+            $and : [
+                {_id: { $nin : Array.from(hideUsersFromFeed)}},
+                {_id: { $ne: loggedInUser._id}}            
+            ]           
+        }).select(USER_SAFE_DATA).skip(skip).limit(limit);
+
+        res.send(users)
+    } catch(err) {
+        res.status(400).send({"message": err.message})
     }
 })
 
